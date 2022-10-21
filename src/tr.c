896f3fc958ef4d20b402335f0f0a277469f8b7cc
Nextime <evan.lebihan@ecole-89.com>
2022-10-21 16:13:55 +0200

    df

--- src/tr.c
+++ src/tr.c
@@ -5,9 +5,7 @@ int main (int a, char **b)
 {
     int i = 0;
     int nb = 0;
-    int nb1 = 1;
-    int nb2 = 1;
-    **b = 0
+    **b = 0;
     char *buffer = malloc(sizeof(char) * 10);
     int size_read = read(0, buffer, 10);
     buffer[size_read] = '\0';
@@ -15,18 +13,18 @@ int main (int a, char **b)
         return 0;
     if (size_read == -1)
         return 0;
-    while (nb != size_read)
+    while (size_read >  0)
         {
             while (buffer[i] != '\0')
                 {
-                    if (buffer[i] == *b[nb1])
+                    if (buffer[i] == b[1][nb])
                         {
-                            buffer[i] = *b[nb2];
+                        buffer[i] = b[2][nb];
                         }
                     write (1, &buffer[i], 1);
                     i = i + 1;
+                    nb = nb + 1;
                 }
-            nb = nb + 1;
         }
     free(buffer);
     return a;
