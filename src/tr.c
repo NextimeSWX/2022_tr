ee19b8d9179c602493a928bb21c9e097d2369453
Nextime <evan.lebihan@ecole-89.com>
2022-10-24 10:52:36 +0200

    Exo tr

--- src/tr.c
+++ src/tr.c
@@ -1,31 +1,29 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main (int a, char **b)
+
+int i;
+int nb;
+int main(int a, char **b)
 {
-    int i = 0;
-    int nb = 0;
+    nb = 0;
     **b = 0;
     char *buffer = malloc(sizeof(char) * 10);
     int size_read = read(0, buffer, 10);
     buffer[size_read] = '\0';
-    if (!buffer)
+    if (!buffer || size_read == -1)
         return 0;
-    if (size_read == -1)
-        return 0;
-    while (size_read >  0)
-        {
-            while (buffer[i] != '\0')
-                {
-                    if (buffer[i] == b[1][nb])
-                        {
-                        buffer[i] = b[2][nb];
-                        }
-                    write (1, &buffer[i], 1);
-                    i = i + 1;
-                    nb = nb + 1;
-                }
+    while (b[1][nb] != '\0') {
+        i = 0;
+        while (buffer[i] != '\0') {
+            if (buffer[i] == b[1][nb]) {
+                buffer[i] = b[2][nb];
+            }
+            write(1, &buffer[i], 1);
+            i = i + 1;
         }
+        nb = nb + 1;
+    }
     free(buffer);
     return a;
 }
